55398c74f7c8801576c60c83a9900f0e5661073c
prasadsawai <98prasadsawai@gmail.com>
2023-01-08 17:15:28 +0530

    Program for PrimeNumber by using dynamic input

--- src/com/sawai/PrimeNumber.java
+++ src/com/sawai/PrimeNumber.java
@@ -1,10 +1,14 @@
 package com.sawai;
 
+import java.util.Scanner;
+
 public class PrimeNumber {
 
 	public static void main(String[] args) {
 		
-		int num = 7;
+		System.out.println("Enter the number");
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
 		int count = 0;
 		for(int i=1; i<=num; i++) {
 			if(num%i==0) {
